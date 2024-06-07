cc7b1d12c76030484264ea174d3403871a515655
CharlyPalm <palmiericarlos@outlook.com>
2024-06-06 19:40:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.header("Hola mundo")
-st.title("La clase de Ivan es la mejor")
+st.title("La clase de Ivan es la mejor, lástima que lo van a correr :(")
 st.markdown("**Holaaaaaaaa**")
